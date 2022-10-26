82d667eab2b2224c8ded0279a3d07087843c19d9
Neelan Pather <97616162+neelan-elucidate@users.noreply.github.com>
2022-10-26 09:29:37 +0000

    Oct 26, 2022, 11:29 AM

--- hello_world.py
+++ hello_world.py
@@ -3,16 +3,13 @@ https://kedro.readthedocs.io/en/stable/get_started/hello_kedro.html
 
 illustrates kedro concepts
 """
-
 # node - wrapper for python function w/ named inputs, outputs
 from kedro.pipeline import node
 from kedro.pipeline import pipeline
 
-
 # Prepare first node
 def return_greeting():
     return "Hello"
-
 
 node1 = node(func=return_greeting, inputs=None, outputs="my_salutation")
 
@@ -20,9 +17,17 @@ node1 = node(func=return_greeting, inputs=None, outputs="my_salutation")
 def join_statements(greeting):
     return f"{greeting} Kedro!"
 
-
 node2 = node(
-    join_statements, inputs="my_salutation", outputs="my_message"
-)
+    join_statements, inputs="my_salutation", outputs="my_message")
+
+"""pipeline
+* organises dependencies & execution order of collection of nodes
+* connects inputs to outputs --> keeps code modular! 
+* pipeline determines node execution order by resolving dependencies 
+* <!> does not necessarily run nodes in order passed 
+"""
+
+
 greeting_pipeline = pipeline([node1, node2])
+
 print("fin")
