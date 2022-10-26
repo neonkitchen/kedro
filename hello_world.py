3e078ebd84b4310650775e264eedd1790a2c9b05
Neelan Pather <97616162+neelan-elucidate@users.noreply.github.com>
2022-10-26 10:39:09 +0000

    Oct 26, 2022, 12:39 PM

--- hello_world.py
+++ hello_world.py
@@ -3,34 +3,41 @@ https://kedro.readthedocs.io/en/stable/get_started/hello_kedro.html
 
 illustrates kedro concepts
 """
-# node - wrapper for python function w/ named inputs, outputs
-from kedro.pipeline import node
-from kedro.pipeline import pipeline
 
-# Prepare first node
+"""
+node 
+* wrapper for python function w/ named inputs, outputs
+"""
+from kedro.pipeline import node
+
+
+# prepare first node
 def return_greeting():
     return "Hello"
 
 node1 = node(func=return_greeting, inputs=None, outputs="my_salutation")
 
-# Prepare second node
+# prepare second node
 def join_statements(greeting):
     return f"{greeting} Kedro!"
 
 node2 = node(
     join_statements, inputs="my_salutation", outputs="my_message")
 
-"""pipeline
+"""
+pipeline
 * organises dependencies & execution order of collection of nodes
 * connects inputs to outputs --> keeps code modular! 
 * pipeline determines node execution order by resolving dependencies 
 * <!> does not necessarily run nodes in order passed 
 """
+from kedro.pipeline import pipeline
 greeting_pipeline = pipeline([node1, node2])
 
-"""DataCatalog
+"""
+DataCatalog
 * registry of all data sources project can use
-* maps names of node inputs, outputs as keys in DataSet (Kedro class - specialised for different type of data stroage)
+* maps names of node inputs, outputs as keys in DataSet (Kedro class - specialised for different type of data storage)
 """
 
 from kedro.io import DataCatalog, MemoryDataSet # Kedro uses a MemoryDataSet for data that is simply stored in-memory
@@ -38,6 +45,12 @@ from kedro.io import DataCatalog, MemoryDataSet # Kedro uses a MemoryDataSet for
 # Prepare a data catalog
 data_catalog = DataCatalog({"my_salutation": MemoryDataSet()})
 
+"""
+Runner
+* object that runs  pipeline
+* Kedro resolves the order in which the nodes are executed:
+"""
+from kedro.runner import SequentialRunner
 
 
 print("fin")
