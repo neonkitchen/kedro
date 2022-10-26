61942804794961bd765e2c55e5f5f2f263848617
Neelan Pather <97616162+neelan-elucidate@users.noreply.github.com>
2022-10-26 08:44:35 +0000

    Oct 26, 2022, 10:44 AM

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,15 @@ https://kedro.readthedocs.io/en/stable/get_started/hello_kedro.html
 illustrates kedro concepts
 """
 
+# node - wrapper for python function w/ named inputs, outputs
+from kedro.pipeline import node
+
+
+# Prepare first node
+def return_greeting():
+    return "Hello"
+
+
+return_greeting_node = node(func=return_greeting, inputs=None, outputs="my_salutation")
+
+print("fin")
